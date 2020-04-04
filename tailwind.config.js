6599f7c3f7bd03f93f65d0993c6e57299fd2e0d7
Emma Stotz <stotz.emma@gmail.com>
2020-04-04 11:48:23 -0400

    feat: extend color palette with brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        primeBlue: "#2e7fad",
+        darkBlue: "#174057",
+        gray: "#f2f2f2"
+      }
+    }
+  },
   variants: {},
   plugins: []
-}
+};
